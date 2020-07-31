6773d49896be13f700c5fa41cb7ec5964c1821eb
Joshua Byrd <phocks@gmail.com>
2020-08-01 06:57:02 +1000

    Updating search and retweet behavior

--- src/actions/searchAndRetweet.ts
+++ src/actions/searchAndRetweet.ts
@@ -13,6 +13,7 @@ export default async (
 
   const twitter = new Twitter(keys);
 
+  // Search for our search phrase
   const [resultError, result] = await to(
     twitter.get("search/tweets", {
       q: searchPhrase,
@@ -26,12 +27,57 @@ export default async (
     return;
   }
 
+  // Get list of userIds the user has blocked
+  const [blockedError, blocked] = await to(
+    twitter.get("blocks/ids", { stringify_ids: true })
+  );
+
+  let blockedIds = [];
+
+  if (blockedError) console.error(blockedError);
+  if (blocked) {
+    console.log("Got blocked accounts. Will not retweet them...");
+    blockedIds = blocked.ids;
+  }
+
+  // Get a list of already tweeted tweets
+  const [timelineError, timelineTweets] = await to(
+    twitter.get("statuses/user_timeline", {
+      include_rts: true,
+      trim_user: true,
+    })
+  );
+
+  if (timelineError) console.error(timelineError);
+
+  let tweetedIds = [];
+
+  if (timelineTweets) {
+    console.log(
+      "Got already tweeted retweets. Will not try to retweet them..."
+    );
+    tweetedIds = timelineTweets.map(
+      (tweet: any) => tweet.retweeted_status.id_str
+    );
+  }
+
+  // Loop through found tweets
   for (const status of result.statuses) {
+    const userIdStr = status.user.id_str;
     const screenName = status.user.screen_name;
     const tweetId = status.id_str;
 
-    // Don't process if in block list
-    if (blockedUsernames.includes(screenName)) {
+    // If already retweeted go to next one
+    if (tweetedIds.includes(tweetId)) {
+      console.log(`Skipping already retweeted tweet by ${screenName}...`);
+      continue;
+    }
+
+    if (
+      blockedIds.includes(userIdStr) ||
+      blockedUsernames.includes(screenName)
+    ) {
+      // Don't process if in block lists
       console.log(`User ${screenName} in block list. Will not retweet...`);
       continue;
     }
--- src/index.ts
+++ src/index.ts
@@ -10,27 +10,11 @@ import searchAndRetweet from "./actions/searchAndRetweet";
 import { myKeys, cowspriactyBotKeys } from "./keys";
 
 // Cowspiracybot blocklist
-const blockedUsernames = [
-  "SSF_BERF_DEFM",
-  "DefendingBeef",
-  "cowspiracybot",
-  "FyfeHendrie",
-  "Jamesrabbit7",
-  "anneroberts_au",
-  "farming_truth_",
-  "NatStoppard",
-  "DougCookRD",
-  "EOMovement",
-  "LA_CHEFs",
-  "RuthMcMScott",
-  "BioMickWatson",
-  "REGENETARIANISM",
-  "corner_mandi"
-];
+const blockedUsernames = ["cowspiracybot"];
 
 const main = async () => {
   // Adds latest @replies & @mentions to a list
-  await addInteractionsToList("1280669347413848064", myKeys);
+  // await addInteractionsToList("1280669347413848064", myKeys);
 
   // Searches a phrase and retweets those tweets
   await searchAndRetweet(
