5561b6c6a2ba616c582c4849a3b1423df20e4102
Joshua Byrd <phocks@gmail.com>
2020-08-15 08:53:17 +1000

    Start of something

--- src/actions/detectNotFollowing.ts
+++ src/actions/detectNotFollowing.ts
@@ -1,9 +1,31 @@
 import Twitter from "twitter-lite";
+import to from "await-to-js";
+
+const appRoot = require("app-root-path");
+
+// Set up a local database
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync(`${appRoot}/data/detectNotFollowing.json`);
+const db = low(adapter);
+
+db.defaults({ friendsList: [] }).write();
 
 export default async (keys: any) => {
   const twitter = new Twitter(keys);
 
-  const result = await twitter.get("friends/ids")
+  let [error, result] = await to(twitter.get("friends/ids"));
 
-  console.log(result)
-}
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const idsAsStrings = result.ids.map((id: number) => id.toString());
+
+  db.set("friendsList", idsAsStrings).write();
+
+  idsAsStrings.forEach((element: string) => {
+    console.log(element);
+  });
+};
